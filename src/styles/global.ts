fa295f14e32ef176b223fdca19a339fa4f2a419f
Artur Silvestre <artur.silvestreo@gmail.com>
2021-07-16 18:08:51 -0300

    Feito Estilos Globais da aplicacao

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f5f5f5;
+    --Blue: #02888E;
+    --Black: #393E46;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    background: var(---background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Archivo', sans-serif;
+    font-weight: 400;
+    outline: none;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }  
+`
