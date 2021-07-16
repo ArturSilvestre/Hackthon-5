ff86cbcb2d8f64ed9c1cfccc131014ccf56bb5da
Artur Silvestre <artur.silvestreo@gmail.com>
2021-07-16 18:11:06 -0300

    Criado a pagina de Login e Estilzado

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,31 @@
+import { Container, Content, Background } from './styles';
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import LogoSignIn from '../../assets/logo.svg';
+
+import { Input }  from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export function SignIn() {
+  return (
+    <Container>
+      <Content>
+        <form> 
+          <h3>Acesso <br /><span>Administrativo</span></h3>
+          <h1>Seja bem-vindo!</h1>
+          <strong>1. Informe seu email e senha</strong>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+      </Content>
+
+      <Background>
+        <img src={LogoSignIn} alt="" />
+      </Background>
+    </Container>
+  )
+}
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+import { lighten } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  max-width: 80%;
+  height: 100vh;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 700px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: left;
+
+    h3 {
+      text-align: center;
+      font-family: 'Archivo', sans-serif;
+      font-weight: 700;
+      font-size: 2.1rem;
+      color: var(--Black);
+      margin-bottom: 10px;
+
+      span {
+        color: var(--Blue);
+      }
+    }
+
+    h1 {
+      color: var(--Black);
+      margin-bottom: 24px;
+      text-align: center;
+      font-size: 4.1rem;
+    }
+
+    a {
+      display: block;
+      font-family: 'Inter', sans-serif;
+      width: 100%;
+      text-align: center;
+      text-decoration: none;
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 25px;
+      color: var(--Black);
+    }
+
+    strong {
+      font-family: 'Inter',sans-serif;
+      font-size: 1.2rem;
+      font-weight: 600;
+      color:var(--Black);
+      text-align: left;
+      margin-bottom: 12px;
+      display: block;
+    }
+  }
+
+`;
+
+export const Background = styled.div`
+display: flex;
+  img {
+    width: 90%;
+  }
+`;
