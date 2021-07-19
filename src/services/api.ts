ec4aeb2096274f3cc3496596eed6ff6056803250
Helil Barbosa Bergária <helil.barbosa@work.co>
2021-07-19 13:30:03 -0300

    feat: Added api delay in dev

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
 
+function sleep(ms = 1000): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const api = axios.create({
   baseURL: 'http://localhost:3000/',
   validateStatus: () => true,
+});
+
+api.interceptors.response.use(async response => {
+  // add artificial delay for dev env
+  if (process.env.NODE_ENV === 'development') {
+    await sleep();
+  }
+
+  return response;
 });
 
 export default api;
