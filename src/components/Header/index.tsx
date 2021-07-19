e02a7d9ddfa23ef265ecd83671e4a5a7c20d99bf
Helil Barbosa Bergária <helil.barbosa@work.co>
2021-07-19 12:36:41 -0300

    feat: Added dynamic Header title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
+import { useMemo } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/AuthContext';
 import { Container, Content } from './styles';
 
 export default function Header(): JSX.Element {
   const { user } = useAuth();
+  const { pathname } = useLocation();
+
+  const title = useMemo(() => {
+    switch (pathname) {
+      case '/cadastrar':
+        return 'Cadastrar nova conta';
+
+      default:
+        return 'Página inicial';
+    }
+  }, [pathname]);
 
   return (
     <Container>
       <Content>
-        <h1>Página Inicial</h1>
+        <h1>{title}</h1>
 
         <span>
           {`${user.first_name} ${user.last_name}`} <FaUserAlt size={24} />
